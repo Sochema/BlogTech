43f9a09063aef26161e07c40fd1f119e7f9a63be
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-06-23 13:32:35 +0200

    alt afficher avec JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /* AFFICHER DATE*/
 
-var d = new Date ();
+var d = new Date();
 var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
 var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 var i = 0;
+
 function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 var minutes = addZero(d.getMinutes());
 document.getElementById("date").innerHTML = days[d.getDay()] + " " + d.getDate() + " " + mois[d.getMonth()] + " " + d.getFullYear() + " - " + d.getHours() + ":" + minutes;
@@ -24,14 +25,19 @@ var text = document.getElementsByClassName("myImg");
      text[i].textContent = alt;
    }
    );
+   img[i].addEventListener("mouseout", function() {
+     text[i].textContent = "";
+   }
+   );
  };
 
-/* COMPTEUR SECONDES */
-var seconds = 0;
-var el = document.getElementById('seconds-counter');
 
-function incrementSeconds() {
-  seconds += 1;
-}
+  /* COMPTEUR SECONDES */
+  var seconds = 0;
+  var el = document.getElementById('seconds-counter');
 
-var cancel = setInterval(incrementSeconds, 1000);
+  function incrementSeconds() {
+    seconds += 1;
+  }
+
+  var cancel = setInterval(incrementSeconds, 1000);
